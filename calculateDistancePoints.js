561f2c81ea7a716dd782347dadf8c2dc4412889b
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-24 20:43:51 +0200

    refactor

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,46 +1,27 @@
 // czy może być calcDistancePoints(normal) zamiast 'normal'?
 
 const calcDistancePoints = (distance, hillSize, kPoint) => {
+
   let points = 0;
 
-  if (hillSize === 'normal' || hillSize === 'big') {
-    points += 60;
-    if (hillSize === 'normal') {
-      if (distance > kPoint) {
-        for (let i = kPoint; i < distance; i += 0.5) {
-          points += 1;
-        }
-      } else if (distance < kPoint) {
-        for (let i = distance; i < kPoint; i += 0.5) {
-          points -= 1;
-        }
-      }
-    } else if (hillSize === 'big') {
-      if (distance > kPoint) {
-        for (let i = kPoint; i < distance; i += 0.5) {
-          points += 0.9;
-        }
-      } else if (distance < kPoint) {
-        for (let i = distance; i < kPoint; i += 0.5) {
-          points -= 0.9;
-        }
-      }
-    }
-  } else if (hillSize === 'mammoth') {
-    points += 120;
-    if (distance > kPoint) {
-      for (let i = kPoint; i < distance; i += 0.5) {
-        points += 0.6;
-      }
-    } else if (distance < kPoint) {
-      for (let i = distance; i < kPoint; i += 0.5) {
-        points -= 0.6;
-      }
-    }
+  switch (hillSize) {
+    case "normal":
+      points = 60;
+      points = points + (distance - kPoint) * 2;
+      break;
+
+    case "big":
+      points = 60;
+      points = points + (distance - kPoint) * 1.8;
+      break;
+
+    case "mammoth":
+      points = 120;
+      points = points + (distance - kPoint) * 1.2;
+      break;
   }
 
-  const pointsForDistance = points.toFixed(1);
-  return pointsForDistance;
+  return points;
 }
 
 module.exports = calcDistancePoints;
