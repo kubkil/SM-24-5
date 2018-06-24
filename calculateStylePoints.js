c42e48949ef4bd86587396146d4925cffc69461e
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-24 20:45:12 +0200

    refactor

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,15 +1,27 @@
-const calcStylePoints = (...stylePoints) => {
-  let pointsForStyle = [...stylePoints];
-  const maxIndex = pointsForStyle.indexOf(Math.max(...pointsForStyle));
-  const minIndex = pointsForStyle.indexOf(Math.min(...pointsForStyle));
+const calcStylePoints = styleNotes => {
+  const maxIndex = styleNotes.indexOf(Math.max(...styleNotes));
+  const minIndex = styleNotes.indexOf(Math.min(...styleNotes));
 
-  pointsForStyle.splice(maxIndex, 1);
-  pointsForStyle.splice(minIndex, 1);
+  styleNotes.splice(maxIndex, 1);
+  styleNotes.splice(minIndex, 1);
 
-  const pointsSum = pointsForStyle.reduce((acc, current) => acc + current);
+  const pointsSum = styleNotes.reduce((acc, current) => acc + current);
   return pointsSum;
 }
 
 module.exports = calcStylePoints;
 
 // dlaczego w package test:style nie działał - do zapytania we wtorek
+
+// const calcStylePoints = (...styleNotes) => {
+//   let pointsForStyle = [...styleNotes];
+//   console.log(pointsForStyle);
+//   const maxIndex = pointsForStyle.indexOf(Math.max(...pointsForStyle));
+//   const minIndex = pointsForStyle.indexOf(Math.min(...pointsForStyle));
+
+//   pointsForStyle.splice(maxIndex, 1);
+//   pointsForStyle.splice(minIndex, 1);
+
+//   const pointsSum = pointsForStyle.reduce((acc, current) => acc + current);
+//   return pointsSum;
+// }
