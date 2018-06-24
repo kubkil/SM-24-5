05a09133a33121bdc09bc32ee4074e7b3b4900d1
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-24 16:03:33 +0200

    refactor

--- calculateStylePoints.test.js
+++ calculateStylePoints.test.js
@@ -2,26 +2,24 @@ const assert = require('assert');
 const calcStylePoints = require('./calculateStylePoints');
 
 describe('calcStylePoints', () => {
-  describe('style', () => {
-    it('should return 49.5', () => {
-      const actual = calcStylePoints(16, 17, 16.5, 16, 18);
-      const expected = 49.5;
+  it('should return 49.5', () => {
+    const actual = calcStylePoints(16, 17, 16.5, 16, 18);
+    const expected = 49.5;
 
-      assert.equal(actual, expected);
-    });
+    assert.equal(actual, expected);
+  });
 
-    it('should return 54.5', () => {
-      const actual = calcStylePoints(16, 17, 17.5, 20, 20);
-      const expected = 54.5;
+  it('should return 54.5', () => {
+    const actual = calcStylePoints(16, 17, 17.5, 20, 20);
+    const expected = 54.5;
 
-      assert.equal(actual, expected);
-    });
+    assert.equal(actual, expected);
+  });
 
-    it('should return 44.5', () => {
-      const actual = calcStylePoints(0, 10, 16.5, 20, 18);
-      const expected = 44.5;
+  it('should return 44.5', () => {
+    const actual = calcStylePoints(0, 10, 16.5, 20, 18);
+    const expected = 44.5;
 
-      assert.equal(actual, expected);
-    });
+    assert.equal(actual, expected);
   });
 });
