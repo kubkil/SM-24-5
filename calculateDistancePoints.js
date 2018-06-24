916c67072f15a6f4938d0b1226f52fbe9daff013
Jakub Kilar <kuba.kilar@gmail.com>
2018-06-24 19:18:47 +0200

    finish function for calc distance function

---
+++ calculateDistancePoints.js
@@ -0,0 +1,44 @@
+// czy może być calcDistancePoints(normal) zamiast 'normal'?
+
+const calcDistancePoints = (distance, hillSize, kPoint) => {
+  let points = 0;
+
+  if (hillSize === 'normal' || hillSize === 'big') {
+    points += 60;
+    if (hillSize === 'normal') {
+      if (distance > kPoint) {
+        for (let i = kPoint; i < distance; i += 0.5) {
+          points += 1;
+        }
+      } else if (distance < kPoint) {
+        for (let i = distance; i < kPoint; i += 0.5) {
+          points -= 1;
+        }
+      }
+    } else if (hillSize === 'big') {
+      if (distance > kPoint) {
+        for (let i = kPoint; i < distance; i += 0.5) {
+          points += 0.9;
+        }
+      } else if (distance < kPoint) {
+        for (let i = distance; i < kPoint; i += 0.5) {
+          points -= 0.9;
+        }
+      }
+    }
+  } else if (hillSize === 'mammoth') {
+    points += 120;
+    if (distance > kPoint) {
+      for (let i = kPoint; i < distance; i += 0.5) {
+        points += 0.6;
+      }
+    } else if (distance < kPoint) {
+      for (let i = distance; i < kPoint; i += 0.5) {
+        points -= 0.6;
+      }
+    }
+  }
+  return points.toFixed(1);
+}
+
+module.exports = calcDistancePoints;
